d3f3a156174fb9d059914ef670dd99914a52e753
wangyang <wangyang5760@163.com>
2017-03-30 10:40:48 +0800

    first commit

---
+++ src/main/com/gcw/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package main.com.gcw.test;
+
+/**
+ * Created by wy on 2017/3/30.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
